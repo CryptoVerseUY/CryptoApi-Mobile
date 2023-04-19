ad15ab61381c4d02a8398c94015a68e8b4dadcde
Ignacio Rios Lahore <ignaciorioslahore@gmail.com>
2023-04-19 00:34:49 -0300

    Agregó funcional de paypal

--- index.js
+++ index.js
@@ -47,7 +47,82 @@ app.use('/success', (req, res) => {
 // Agrega credenciales
 mercadopago.configure({
     // INGRESAR TOKEN
-    access_token: "",
+    access_token: "APP_USR-743201864000985-041223-687991e5c9053ecb79f5332d1d795909-366379713",
+});
+
+
+
+
+//IMPLEMENTACION DE API DE PAYPAL
+const clientId = 'AXYzuKmYH9O2XFjcQD0c5mqEeC_dZEpEqg1Yb9_2Km5HziNNiZpP7DxFWDIfP-e6Gs8OOSrFx50WVHCo';
+const clientSecret = 'EDACidqNr6qTeU9HAIygNq1664iutHGKDjt_q6Bs_wQcTqp8joCq6rM5hba01maMqMdTpTQzw9ORkjMO';
+const baseUrl = 'https://api.sandbox.paypal.com'; // Cambiar a 'https://api.paypal.com' en producción
+
+// Ruta para crear un pago
+app.get('/create-payment', (req, res) => {
+    const payment = {
+        intent: 'sale',
+        payer: {
+            payment_method: 'paypal'
+        },
+        redirect_urls: {
+            return_url: 'http://localhost:3000/success', // URL de retorno en caso de éxito
+            cancel_url: 'http://localhost:3000/cancel' // URL de retorno en caso de cancelación
+        },
+        transactions: [
+            {
+                amount: {
+                    total: '10.00', // Monto del pago
+                    currency: 'USD' // Moneda del pago
+                },
+                description: 'Ejemplo de pago con PayPal' // Descripción del pago
+            }
+        ]
+    };
+
+    request.post(
+        `${baseUrl}/v1/payments/payment`,
+        {
+            auth: {
+                user: clientId,
+                pass: clientSecret
+            },
+            body: payment,
+            json: true
+        },
+        (error, response, body) => {
+            if (error) {
+                res.status(500).send({ error: 'Error al crear el pago' });
+            } else {
+                res.send({ approvalUrl: body.links.find(link => link.rel === 'approval_url').href });
+            }
+        }
+    );
+});
+
+// Ruta para ejecutar un pago
+app.get('/execute-payment/:paymentId/:payerId', (req, res) => {
+    const paymentId = req.params.paymentId;
+    const payerId = req.params.payerId;
+
+    request.post(
+        `${baseUrl}/v1/payments/payment/${paymentId}/execute`,
+        {
+            auth: {
+                user: clientId,
+                pass: clientSecret
+            },
+            body: { payer_id: payerId },
+            json: true
+        },
+        (error, response, body) => {
+            if (error) {
+                res.status(500).send({ error: 'Error al ejecutar el pago' });
+            } else {
+                res.send({ payment: body });
+            }
+        }
+    );
 });
 
 
